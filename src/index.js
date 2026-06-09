a6bf73881a60fa72d19b9755902237b88a7334cf
Ilya Danilov <mc.nom@icloud.com>
2026-06-09 16:50:30 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const runGame = (description, generateRound) => {
 
         if (userAnswer !== correctAnswer) {
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`)
-            console.log(`Let's try again, ${userName}`)
+            console.log(`Let's try again, ${userName}!`)
             return
         }
 
